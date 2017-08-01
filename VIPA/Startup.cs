5f7010df6aad3aec7ae0bfc1df84de7768277697
PadmajaShukla <padmaja.shukla@gmail.com>
2017-08-01 10:05:06 +0530

    First Commit

---
+++ VIPA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VIPA.Startup))]
+namespace VIPA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
